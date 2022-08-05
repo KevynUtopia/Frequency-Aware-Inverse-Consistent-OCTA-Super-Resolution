05fbb029b4c65ca3b1b4144a06ab23f39f3ceb84
Kevyn ZHANG <wzhangbu@connect.ust.hk>
2022-08-05 22:28:46 +0800

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import torch
-from model import UnetGeneratorA2B
-
-netG_A2B = torch.load('./output_exp/netG_A2B_epoch60.pth')
-# type(netG_A2B)
-model = UnetGeneratorA2B(opt.output_nc, opt.input_nc).cuda()
-model.load_state_dict(netG_A2B, strict=False)
